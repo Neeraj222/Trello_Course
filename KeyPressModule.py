87535e623f77f4da7e69ee45ff431c9f003f886d
Neeraj <49423198+Neeraj222@users.noreply.github.com>
2021-08-21 17:02:43 +1000

    Basic Keyboard Functionality

---
+++ KeyPressModule.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+def init():
+    pygame.init()
+    win = pygame.display.set_mode((400, 400))
+
+def getKey(keyName):
+    ans = False
+    for eve in pygame.event.get(): pass
+    keyInput = pygame.key.get_pressed()
+    mKey = getattr(pygame, 'K_{} '.format(keyName))
+    if keyInput[mKey]:
+        ans = True
+    pygame.display.update()
+    return ans
+
+def main():
+    if getKey("LEFT"):
+        print("left key pressed")
+        if getKey("RIGHT"):
+            print("right key pressed")
+        
+    print(getKey("a"))
+
+
+
+
+
+if __name__ == '__main__':
+    init()
+    while True:
+        main()
